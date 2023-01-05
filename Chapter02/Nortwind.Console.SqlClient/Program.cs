3094a85702c9cf7c36845039f95108ed2545b0ac
Juan de Dios <juan.gomez@trip-arc.com>
2023-01-05 09:07:43 -0500

    testing Storage Procedure with ADO.NET

--- Chapter02/Nortwind.Console.SqlClient/Program.cs
+++ Chapter02/Nortwind.Console.SqlClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Reflection.Metadata;
 
 SqlConnectionStringBuilder builder = new();
 builder.InitialCatalog = "Northwind";
@@ -105,10 +106,54 @@ if (!decimal.TryParse(priceText, out decimal price))
     return;
 }
 
-cmd.CommandType = CommandType.Text;
-cmd.CommandText = "SELECT ProductId, ProductName, UnitPrice FROM Products"
-    + " WHERE UnitPrice > @price";
-cmd.Parameters.AddWithValue("price", price);
+WriteLine("Execute command using :");
+WriteLine("   1 - Text");
+WriteLine("   2 - Stored Procedure");
+WriteLine();
+Write("Press a Key: ");
+
+key = ReadKey().Key;
+WriteLine(); WriteLine();
+
+SqlParameter p1, p2 = new(), p3 = new();
+
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    cmd.CommandType = CommandType.Text;
+    cmd.CommandText = "SELECT ProductId, ProductName, UnitPrice FROM Products"
+                      + " WHERE UnitPrice > @price";
+    cmd.Parameters.AddWithValue("price", price);
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.CommandText = "GetExpensiveProducts";
+
+    p1 = new()
+    {
+        ParameterName = "price",
+        SqlDbType = SqlDbType.Money,
+        SqlValue = price
+    };
+    
+    p2 = new()
+    {
+        Direction = ParameterDirection.Output,
+        ParameterName = "count",
+        SqlDbType = SqlDbType.Int
+    };
+    
+    p3 = new()
+    {
+        Direction = ParameterDirection.ReturnValue,
+        ParameterName = "rv",
+        SqlDbType = SqlDbType.Int
+    };
+
+    cmd.Parameters.Add(p1);
+    cmd.Parameters.Add(p2);
+    cmd.Parameters.Add(p3);
+}
 
 // SqlDataReader r = cmd.ExecuteReader();
 SqlDataReader r = await cmd.ExecuteReaderAsync();
@@ -130,4 +175,8 @@ while (await r.ReadAsync())
 }
 
 await r.CloseAsync();
+
+WriteLine($"Output count: {p2.Value}");
+WriteLine($"Retun value: {p3.Value}");
+
 await connection.CloseAsync();
