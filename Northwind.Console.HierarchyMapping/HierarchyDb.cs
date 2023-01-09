0f8b03cfcd7f31f8bd99600e70843ded5f7c893c
jddios837 <jddios837@gmail.com>
2023-01-09 11:02:03 -0500

    testing TPH mapping strategy

---
+++ Northwind.Console.HierarchyMapping/HierarchyDb.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Console.HierarchyMapping;
+
+public class HierarchyDb : DbContext
+{
+    public DbSet<Person>? People { get; set; }
+    public DbSet<Student>? Students { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+
+    public HierarchyDb(DbContextOptions<HierarchyDb> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Person>().UseTphMappingStrategy();
+
+        Student p1 = new() { Id = 1, Name = "Roman Roy", Subject = "History" };
+        Employee p2 = new()
+        {
+            Id = 2, Name = "Kendall Roy", HireDate = new(year: 2014, month: 4, day: 2)
+        };
+        Employee p3 = new()
+        {
+            Id = 3, Name = "Siobhan Roy", HireDate = new(year: 2020, month: 9, day: 12)
+        };
+
+        modelBuilder.Entity<Student>().HasData(p1);
+        modelBuilder.Entity<Employee>().HasData(p2, p3);
+    }
+}
--- Northwind.Console.HierarchyMapping/Program.cs
+++ Northwind.Console.HierarchyMapping/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Console.HierarchyMapping;
 
-Console.WriteLine("Hello, World!");
+DbContextOptionsBuilder<HierarchyDb> options = new();
+options.UseSqlServer("Data Source=localhost;Initial Catalog=HierarchyMapping;" +
+                     "TrustServerCertificate=true;" +
+                     "User Id=sa;Password=Tetra714217#;");
+
+using (HierarchyDb db = new(options.Options))
+{
+    bool deleted = await db.Database.EnsureDeletedAsync();
+    WriteLine($"Database deleted: {deleted}");
+
+    bool created = await db.Database.EnsureCreatedAsync();
+    WriteLine($"Database created: {created}");
+    
+    WriteLine("SQL script used tp create tje database:");
+    WriteLine(db.Database.GenerateCreateScript());
+
+    if (db.Students is null || !db.Students.Any())
+    {
+        WriteLine("There are no students.");
+    }
+    else
+    {
+        foreach (var s in db.Students)
+        {
+            WriteLine("{0} studies {1}", s.Name, s.Subject);
+        }
+    }
+    
+    if (db.Employees is null || !db.Employees.Any())
+    {
+        WriteLine("There are no employees.");
+    }
+    else
+    {
+        foreach (var e in db.Employees)
+        {
+            WriteLine("{0} studies {1}", e.Name, e.HireDate);
+        }
+    }
+    
+    if (db.People is null || !db.People.Any())
+    {
+        WriteLine("There are no people.");
+    }
+    else
+    {
+        foreach (var p in db.People)
+        {
+            WriteLine("{0} studies {1}", p.Name, p.Id);
+        }
+    }
+}
