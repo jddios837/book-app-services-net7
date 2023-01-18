e3f58f329b9ca0bd2fcd134e16b2262e5b495c09
jddios837 <jddios837@gmail.com>
2023-01-17 20:10:39 -0500

    Working in cosmosdb integration

---
+++ Chapter03/Northwind.CosmosDb.SqlApi/Program.Methods.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace Northwind.CosmosDb.SqlApi;
+
+partial class Program
+{
+    private static string endPointUri = "";
+    private static string primaryKey = "";
+
+    static async Task CreateCosmosResource()
+    {
+        SectionTitle("Creating Cosmos Resource");
+
+        try
+        {
+            using (CosmosClient client = new(accountEndpoint: endPointUri, authKeyOrResourceToken: primaryKey))
+            {
+                DatabaseResponse dbResponse = await client
+                    .CreateDatabaseIfNotExistsAsync("Northwind", throughput: 400);
+
+                string status = dbResponse.StatusCode switch
+                {
+                    HttpStatusCode.OK => "exists",
+                    HttpStatusCode.Created => "created",
+                    _ => "unknown"
+                };
+                
+                WriteLine("Database Id: {0}, Status: {1}", arg0: dbResponse.Database.Id, arg1: status);
+
+                IndexingPolicy indexingPolicy = new()
+                {
+                    IndexingMode = IndexingMode.Consistent,
+                    Automatic = true
+                };
+            }
+        }
+        catch (Exception e)
+        {
+            WriteLine(e);
+            throw;
+        }
+    }
+
+}
