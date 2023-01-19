9192d7e87d81d57d0ba13d4590ef567f619bdb0b
jddios837 <jddios837@gmail.com>
2023-01-19 08:54:28 -0500

    Adding models for cosmos

---
+++ Chapter03/Northwind.CosmosDb.SqlApi/items/CategoryCosmos.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.CosmosDb.SqlApi.items;
+
+public class CategoryCosmos
+{
+    public int categoryId { get; set; }
+    public string categoryName { get; set; } = null!;
+    public string? descrition { get; set; }
+    
+}
---
+++ Chapter03/Northwind.CosmosDb.SqlApi/items/ProductCosmos.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.CosmosDb.SqlApi.items;
+
+public class ProductCosmos
+{
+    public string id { get; set; } = null!;
+    public string productId { get; set; } = null!;
+    public string productName { get; set; } = null!;
+    
+}
