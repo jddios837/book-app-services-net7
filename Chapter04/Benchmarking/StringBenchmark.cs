af2c0574316603a4740ddb0c74c037203a8a747e
jddios837 <jddios837@gmail.com>
2023-01-24 09:11:35 -0500

    Testing with Benchmark

---
+++ Chapter04/Benchmarking/StringBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking;
+
+public class StringBenchmark
+{
+    private int[] _numbers;
+
+    public StringBenchmark()
+    {
+        _numbers = Enumerable.Range(start: 1, count: 20).ToArray();
+    }
+
+    [Benchmark(Baseline = true)]
+    public string StringConcatenationTest()
+    {
+        string s = string.Empty;
+
+        for (int i = 0; i < _numbers.Length; i++)
+        {
+            s += _numbers[i] + ", ";
+        }
+
+        return s;
+    }
+
+    [Benchmark]
+    public string StringBuilderTest()
+    {
+        System.Text.StringBuilder builder = new();
+
+        for (int i = 0; i < _numbers.Length; i++)
+        {
+            builder.Append(_numbers[i]);
+            builder.Append(", ");
+        }
+
+        return builder.ToString();
+    }
+}
