ee97a98e42f1b2dbe4e237326c3121172989c513
jddios837 <jddios837@gmail.com>
2023-01-26 15:58:27 -0500

    Main test of sycn added on Program.cs

--- Chapter04/WorkingWithTasks/Program.cs
+++ Chapter04/WorkingWithTasks/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+namespace WorkingWithTasks;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        OutputThreadInfo();
+        Stopwatch timer = Stopwatch.StartNew();
+        
+        SectionTitle("Running methods sync on one thread");
+        MethodA();
+        MethodB();
+        MethodC();
+        
+        WriteLine($"{timer.ElapsedMilliseconds: #, ##0}ms elapsed.");
+    }
+}
