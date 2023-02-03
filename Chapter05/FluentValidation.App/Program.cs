b9eae944475e481e7eef68e77c567bf8ccd9bb65
jddios837 <jddios837@gmail.com>
2023-02-02 21:26:20 -0500

    Completed validation APP test

--- Chapter05/FluentValidation.App/Program.cs
+++ Chapter05/FluentValidation.App/Program.cs
@@ -1,12 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using FluentValidation.Models;
 using FluentValidation.Results;
 using FluentValidation.Validators;
 
-Order order = new() { };
+Order order = new()
+{
+    OrderId = 10001,
+    CustomerName = "Abcdef",
+    CustomerEmail = "abc@example.com",
+    CustomerLevel = CustomerLevel.Gold,
+    OrderDate = new(2022, 12, 1),
+    ShipDate = new(2022,12,5),
+    Total = 49.99M
+};
 
 OrderValidator validator = new();
 
 ValidationResult result = validator.Validate(order);
 
-Console.WriteLine("Hello, World!");
+WriteLine($"CustomerName: {order.CustomerName}");
+WriteLine($"CustomerEmail: {order.CustomerEmail}");
+WriteLine($"CustomerLevel: {order.CustomerLevel}");
+WriteLine($"OrderId: {order.OrderId}");
+WriteLine($"OrderDate: {order.OrderDate}");
+WriteLine($"ShipDate: {order.ShipDate}");
+WriteLine($"Total: {order.Total}");
+WriteLine();
+WriteLine($"IsValid: {result.IsValid}");
+foreach (var item in result.Errors)
+{
+    WriteLine($" {item.Severity}: {item.ErrorMessage}");
+}
