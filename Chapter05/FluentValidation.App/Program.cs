71386aac45ae6e8b1dc7c910f4c22e2b04fbe3af
jddios837 <jddios837@gmail.com>
2023-02-02 21:11:36 -0500

    Adding validation App

---
+++ Chapter05/FluentValidation.App/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FluentValidation.Models;
+using FluentValidation.Results;
+using FluentValidation.Validators;
+
+Order order = new() { };
+
+OrderValidator validator = new();
+
+ValidationResult result = validator.Validate(order);
+
+Console.WriteLine("Hello, World!");
