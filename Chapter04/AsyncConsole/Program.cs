6ce3256dc0baaa8d6b3d180b51e1c76081949fe5
jddios837 <jddios837@gmail.com>
2023-02-15 12:07:16 -0500

    Async in Main added in .NET 7.1

---
+++ Chapter04/AsyncConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+HttpClient client = new();
+HttpResponseMessage responseMessage = await client.GetAsync("https://www.apple.com/");
+
+WriteLine($"Apple's home page has {0:N0} bytes.", responseMessage.Content.Headers.ContentLength);
