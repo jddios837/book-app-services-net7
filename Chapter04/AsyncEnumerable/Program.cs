152af314a76d0f668319c25f42cf8eaa8a11ae50
jddios837 <jddios837@gmail.com>
2023-02-15 12:17:24 -0500

    Asycn enumerable yield return

---
+++ Chapter04/AsyncEnumerable/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+async static IAsyncEnumerable<int> GetNumberAsync()
+{
+    Random r = Random.Shared;
+    
+    // simulate work 
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+}
+
+await foreach (int number in GetNumberAsync())
+{
+    WriteLine($"Number: {number}");
+}
