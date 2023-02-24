5ddbc48f36b06d60faf765f8b450bb3417d5e6ce
jddios837 <jddios837@gmail.com>
2023-02-24 18:31:34 -0500

    Processing image from normal to thumbnailPath in grayScale

--- Chapter05/WorkingWithImages/Program.cs
+++ Chapter05/WorkingWithImages/Program.cs
@@ -1,3 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
-Console.WriteLine("Hello, World!");
+string imageFolder = Path.Combine(Environment.CurrentDirectory, "images");
+
+WriteLine($"Image folder {imageFolder}");
+WriteLine();
+
+if (!Directory.Exists(imageFolder))
+{
+    WriteLine();
+    WriteLine("Folder does not exist!");
+    return;
+}
+
+IEnumerable<string> images = Directory.EnumerateFiles(imageFolder);
+
+foreach (string imagePath in images)
+{
+    if (Path.GetFileNameWithoutExtension(imagePath).EndsWith("-thumbnail"))
+    {
+        WriteLine($"Skipping:\n {imagePath}");
+        WriteLine();
+        continue; // this files has already been converted
+    }
+
+    var thumbnailPath = Path.Combine(
+        Environment.CurrentDirectory, "images",
+        Path.GetFileNameWithoutExtension(imagePath)
+        + "-thumbnail" + Path.GetExtension(imagePath));
+
+    using (Image image = Image.Load(imagePath))
+    {
+        WriteLine($"Converting:\n {imagePath}");
+        WriteLine($"To:\n {thumbnailPath}");
+        
+        image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+        image.Mutate(x => x.Grayscale());
+        image.Save(thumbnailPath);
+        WriteLine();
+    }
+}
+
+WriteLine("Image processing complete. View the images folder.");
