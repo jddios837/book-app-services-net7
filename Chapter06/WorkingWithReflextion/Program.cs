973f56d3600e54d603d46d82a169dbd8c07397b7
jddios837 <jddios837@gmail.com>
2023-03-07 17:30:38 -0500

    First reflexion test with one assembly

--- Chapter06/WorkingWithReflextion/Program.cs
+++ Chapter06/WorkingWithReflextion/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection; // Assembly
 
-Console.WriteLine("Hello, World!");
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($" Full name: {assembly.FullName}");
+WriteLine($" Location : {assembly.Location}");
+WriteLine($" Entry point: {assembly.EntryPoint?.Name}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine($" Assembly-level attributes:");
+foreach (Attribute attribute in attributes)
+{
+    WriteLine($"         {attribute.GetType()}");
+}
