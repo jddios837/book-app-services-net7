96f9579d9d899d50548f85bf3182f9555916685e
jddios837 <jddios837@gmail.com>
2023-03-18 21:13:04 -0500

    Loading dynamically an assembly

---
+++ Chapter06/DynamicLoadAndExecute.Console/Program.Helper.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+partial class Program
+{
+    static void OutputAssemblyInfo(Assembly a)
+    {
+        WriteLine("FullName: {0}", a.FullName);
+        WriteLine("Location: {0}", Path.GetDirectoryName(a.Location));
+        WriteLine("IsCollectible: {0}", a.IsCollectible);
+        WriteLine("Defined types:");
+        foreach (var info in a.DefinedTypes)
+        {
+            if (!info.Name.EndsWith("Attribute"))
+            {
+                WriteLine("    Name: {0}, Members: {1}", info.Name, info.GetMembers().Length);
+            }
+        }
+        WriteLine();
+    }
+}
