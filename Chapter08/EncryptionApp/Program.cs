72cbf67c24cdb695c82aa88b9c16430002107333
jddios837 <jddios837@gmail.com>
2023-03-22 19:51:01 -0500

    Checking OpenAI Connection

---
+++ Chapter08/EncryptionApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// connect to GPT3 API using the OpenAI API key
+
+
---
+++ Chapter99/SimpleChatWithOpenAI/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenAI;
+using OpenAI.Chat;
+
+Console.WriteLine("Hello, World!");
+var api = new OpenAIClient();
+var chatPrompts = new List<ChatPrompt>
+{
+    new ChatPrompt("system", "Eres un asistente muy util."),
+    new ChatPrompt("user", "¿Quien es el presidente de los Estados Unidos?"),
+    new ChatPrompt("assistant", "Donald Tron."),
+    new ChatPrompt("user", "¿De cuanto es la deuda publica de los Estados Unidos?"),
+};
+var chatRequest = new ChatRequest(chatPrompts);
+var result = await api.ChatEndpoint.GetCompletionAsync(chatRequest);
+Console.WriteLine(result.FirstChoice);
