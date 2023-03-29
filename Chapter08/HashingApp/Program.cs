1fc08af88892a16b3dca75fa8c9eec99b6ef9f87
jddios837 <jddios837@gmail.com>
2023-03-28 19:42:47 -0500

    log in a user using basic encriptation

--- Chapter08/HashingApp/Program.cs
+++ Chapter08/HashingApp/Program.cs
@@ -9,3 +9,61 @@ WriteLine($"   Salt: {alice.Salt}");
 WriteLine($"   Password (salted and hashed): {alice.SaltedHashedPassword}");
 WriteLine();
 
+WriteLine($"Enter a new user to register: ");
+string? username = ReadLine();
+if (string.IsNullOrEmpty(username))
+{
+    username = "Bob";
+}
+
+WriteLine($"Enter a password for {username}: ");
+string? password = ReadLine();
+if (string.IsNullOrEmpty(password))
+{
+    password = "Pa$$w0rd";
+}
+
+WriteLine("Registering a new user:");
+User newUser = Protector.Register(username, password);
+
+WriteLine($"   Name: {newUser.Name}");
+WriteLine($"   Salt: {newUser.Salt}");
+WriteLine($"   Password (salted and hashed): {newUser.SaltedHashedPassword}");
+WriteLine();
+
+bool correctPassword = false;
+
+while (!correctPassword)
+{
+    WriteLine("Enter a username to log in: ");
+    string? loginUserName = ReadLine();
+    if (string.IsNullOrEmpty(loginUserName))
+    {
+        WriteLine("Login username cannot be empty.");
+        Write("Press Ctrl + C to end or press ENTER to retry.");
+        ReadLine();
+        continue;
+    }
+    
+    WriteLine("Enter a password to log in: ");
+    string? loginPassword = ReadLine();
+    if (string.IsNullOrEmpty(loginPassword))
+    {
+        WriteLine("Login password cannot be empty.");
+        Write("Press Ctrl + C to end or press ENTER to retry.");
+        ReadLine();
+        continue;
+    }
+
+    correctPassword = Protector.CheckPassword(loginUserName, loginPassword);
+
+    if (correctPassword)
+    {
+        WriteLine($"Correct! {loginUserName} has been logged in.");
+    }
+    else
+    {
+        WriteLine("Invalid username or password. Try again.");
+    }
+}
+
