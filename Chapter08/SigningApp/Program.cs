70b7444fb050eb771a64b2c2f1eac2e0f4362daa
jddios837 <jddios837@gmail.com>
2023-03-30 05:05:54 -0500

    Signing examples

--- Chapter08/SigningApp/Program.cs
+++ Chapter08/SigningApp/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Packt.Shared;
 
-Console.WriteLine("Hello, World!");
+Write("Enter some text to sign: ");
+string? data = ReadLine();
+
+if (string.IsNullOrEmpty(data))
+{
+    WriteLine("You must enter some text to sign.");
+    return;
+}
+
+string signature = Protector.GenerateSignature(data);
+
+WriteLine($"Signature: {signature}");
+WriteLine("Public key used to signature:");
+WriteLine(Protector.PublicKey);
+
+if (Protector.ValidateSignature(data, signature))
+{
+    WriteLine("Correct! Signature is valid. Data has not been manipulated");
+}
+else
+{
+    WriteLine("Signature is NOT valid.");
+}
+
+string manipulatedData = data.Replace(data[0], 'X');
+if (manipulatedData == data)
+{
+    manipulatedData = data.Replace(data[0], 'Y');
+}
+
+if(Protector.ValidateSignature(manipulatedData, signature))
+{
+    WriteLine("Correct! Signature is valid. Data has not been manipulated");
+}
+else
+{
+    WriteLine($"Signature is NOT valid. Data has been manipulated to: {manipulatedData}");
+}
