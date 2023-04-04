05cbb65aecb0eae4e31d6aeaa8e43b718422d309
jddios837 <jddios837@gmail.com>
2023-04-04 04:27:33 -0500

    Working with auth

--- Chapter08/SecureApp/Program.cs
+++ Chapter08/SecureApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security;
 using Packt.Shared;
 using System.Security.Principal;
 using System.Security.Claims;
@@ -8,4 +10,64 @@ Protector.Register("Bob", "Pa$$w0rd", roles: new string[] { "Sales", "TeamLeads"
 
 Protector.Register("Eva", "Pa$$w0rd");
 
-Console.WriteLine("Hello, World!");
+WriteLine($"Enter your username: ");
+string? username = ReadLine();
+
+WriteLine($"Enter your password: ");
+string? password = ReadLine();
+
+Protector.LogIn(username, password);
+
+if (Thread.CurrentPrincipal == null)
+{
+    WriteLine("Log in failed.");
+    return;
+}
+
+IPrincipal p = Thread.CurrentPrincipal;
+WriteLine($"IsAuthenticated: {p.Identity.IsAuthenticated}");
+WriteLine($"AuthenticationType: {p.Identity.AuthenticationType}");
+WriteLine($"Name: {p.Identity.Name}");
+WriteLine($"IsInRole(\"Admins\"): {p.IsInRole("Admins")}");
+WriteLine($"IsRole(\"Sales\"): {p.IsInRole("Sales")}");
+
+if (p is ClaimsPrincipal)
+{
+    WriteLine($"{p.Identity?.Name} has the following claims:");
+    
+    IEnumerable<Claim>? claims = (p as ClaimsPrincipal)?.Claims;
+
+    if (claims is not null)
+    {
+        foreach (var c in claims)
+        {
+            WriteLine($"{c.Type} : {c.Value}");
+        }
+    }
+}
+
+try
+{
+    SecureFeature();
+}
+catch (Exception e)
+{
+    WriteLine($"{e.GetType()}: {e.Message}");
+    throw;
+}
+
+
+static void SecureFeature()
+{
+    if (Thread.CurrentPrincipal == null)
+    {
+        throw new SecurityException("A user must be logged in to access this feature.");
+    }
+    
+    if (!Thread.CurrentPrincipal.IsInRole("Admins"))
+    {
+        throw new SecurityException("Only users in the Admins role can access this feature.");
+    }
+    
+    WriteLine("You have access to this feature.");
+}
