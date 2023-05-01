30507e69c2123d5476764221c07e7cbbaf774c68
jddios837 <jddios837@gmail.com>
2023-05-01 14:14:31 -0500

    Added Categories Controller using OData

---
+++ Chapter10/Northwind.Odata.Service/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Packt.Shared;
+
+namespace Northwind.Odata.Service.Controllers;
+
+public class CategoriesController : ODataController
+{
+    private readonly NorthwindContext _db;
+
+    public CategoriesController(NorthwindContext db)
+    {
+        this._db = db;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(_db.Categories);
+    }
+
+    public IActionResult Get(int key)
+    {
+        return Ok(_db.Categories.Where(category => category.CategoryId == key));
+    }
+}
