56dc61461445b6303ea6fc2c4766d127d2a600bc
jddios837 <jddios837@gmail.com>
2023-05-02 18:23:41 -0500

    Added other OData Controllers

---
+++ Chapter10/Northwind.Odata.Service/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Packt.Shared;
+
+namespace Northwind.Odata.Service.Controllers;
+
+public class ProductsController : ODataController
+{
+    private readonly NorthwindContext _db;
+
+    public ProductsController(NorthwindContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(_db.Products);
+    }
+
+    public IActionResult Get(int key)
+    {
+        return Ok(_db.Products.Where(product => product.ProductId == key));
+    }
+}
---
+++ Chapter10/Northwind.Odata.Service/Controllers/Suppliers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Packt.Shared;
+
+namespace Northwind.Odata.Service.Controllers;
+
+public class Suppliers : ODataController
+{
+    private readonly NorthwindContext _db;
+    
+    public Suppliers(NorthwindContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(_db.Suppliers);
+    }
+
+    public IActionResult Get(int key)
+    {
+        return Ok(_db.Suppliers.Where(supplier => supplier.SupplierId == key));
+    }
+}
