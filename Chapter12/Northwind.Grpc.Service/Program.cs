204f2fed116544086298b0bc405f93b62b64fca8
jddios837 <jddios837@gmail.com>
2023-09-15 18:55:25 -0500

    Add new endpoint to consult DB in gRPC

--- Chapter12/Northwind.Grpc.Service/Program.cs
+++ Chapter12/Northwind.Grpc.Service/Program.cs
@@ -1,4 +1,5 @@
 using Northwind.Grpc.Service.Services;
+using Packt.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddNorthwindContext();
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ShipperService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
---
+++ Chapter12/Northwind.Grpc.Service/Services/ShipperService.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using Packt.Shared;
+using ShipperEntity = Packt.Shared.Shipper;
+
+namespace Northwind.Grpc.Service.Services;
+
+public class ShipperService : Shipper.ShipperBase
+{
+    protected readonly ILogger<ShipperService> _logger;
+    protected readonly NorthwindContext db;
+
+    public ShipperService(ILogger<ShipperService> logger, NorthwindContext db)
+    {
+        _logger = logger;
+        this.db = db;
+    }
+
+    public override async Task<ShipperReply?> GetShipper(ShipperRequest request, ServerCallContext context)
+    {
+        ShipperEntity? shipper = await db.Shippers.FindAsync(request.ShipperId);
+
+        if (shipper == null)
+        {
+            return null;
+        }
+        else
+        {
+            return ToShipperReply(shipper);
+        }
+    }
+
+    private ShipperReply ToShipperReply(ShipperEntity shipper)
+    {
+        return new ShipperReply
+        {
+            ShipperId = shipper.ShipperId,
+            CompanyName = shipper.CompanyName,
+            Phone = shipper.Phone
+        };
+    }
+}
