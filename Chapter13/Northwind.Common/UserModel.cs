3bfd465174af9b38edd1d645dad189b0b89b8934
jddios837 <jddios837@gmail.com>
2023-09-23 14:53:37 -0500

    Initial projects with SignalR

---
+++ Chapter13/Northwind.Common/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Common;
+
+public class UserModel
+{
+    public string Name { get; set; } = null!;
+    public string ConnectionId { get; set; } = null!;
+    public string? Groups { get; set; } // comma-separe list
+}
