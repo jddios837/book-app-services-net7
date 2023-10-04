28bb978633c9c895163d1b69b74a98f2958c7808
jddios837 <jddios837@gmail.com>
2023-10-04 15:27:44 -0500

    Adding a SignalR Hub

---
+++ Chapter13/Northwind.SignalR.Client.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Northwind.Common;
+
+Write("Enter a username (required): ");
+string? username = ReadLine();
+
+if (string.IsNullOrWhiteSpace(username))
+{
+    WriteLine("Username is required.");
+    return;
+}
+
+Write("Enter your group name (optional): ");
+string? groupName = ReadLine();
+
+HubConnection connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5132/chat")
+    .Build();
+    
+connection.On<MessageModel>("ReceiveMessage", message =>
+{
+    WriteLine($"To {message.To}, From {message.From}: {message.Body}");
+});
+
+await connection.StartAsync();
+
+WriteLine("Successfully started.");
+
+UserModel user = new UserModel
+{
+    Name = username,
+    Groups = groupName
+};
+
+await connection.InvokeAsync("Register", user);
+
+WriteLine("Successfully registered.");
+WriteLine("Listening for messages. Press any key to exit.");
+ReadLine();
---
+++ Chapter13/Northwind.SignalR.Service.Client.Mvc/Hubs/StockPriceHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Northwind.SignalR.Streams;
+
+namespace Northwind.SignalR.Service.Client.Mvc.Hubs;
+
+public class StockPriceHub : Hub
+{
+    public async IAsyncEnumerable<StockPrice> GetStockPrices(string stockSymbol, CancellationToken cancellationToken)
+    {
+        double currentPrice = 267.10; // Simulated initial price.
+
+        for (int i = 0; i < 10; i++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            currentPrice += (Random.Shared.NextDouble() * 10.0) - 5.0;
+
+            StockPrice stockPrice = new(stockSymbol, currentPrice);
+            
+            Console.WriteLine("[{0}] {1} at {2:C",
+                DateTime.UtcNow, stockPrice.Stock, stockPrice.Price);
+            
+            yield return stockPrice;
+
+            await Task.Delay(4000, cancellationToken);
+        }
+    }
+
+    public async Task UploadStocks(IAsyncEnumerable<string> stocks)
+    {
+        await foreach (string stock in stocks)
+        {
+            Console.WriteLine($"Uploading {stock}...");
+        }
+    }
+}
