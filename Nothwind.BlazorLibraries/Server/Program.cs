ee5550f274bfa8cb6ced4b16013585aa73f610c2
jddios837 <jddios837@gmail.com>
2023-11-03 18:05:14 -0500

    Adding Categories page in Blazor application

--- Nothwind.BlazorLibraries/Server/Program.cs
+++ Nothwind.BlazorLibraries/Server/Program.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Mvc;
+using Packt.Shared;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using HttpJsonOption = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// avoid circular references when serializing objects
+builder.Services.Configure<HttpJsonOption>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddNorthwindContext();
 
 var app = builder.Build();
 
@@ -23,6 +35,19 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// define endpoints for the Minimal API
+app.MapGet("api/categories", ([FromServices] NorthwindContext db) =>
+    Results.Json(
+        db.Categories.Include(c => c.Products)))
+    .WithName("GetCategories")
+    .Produces<Category[]>(StatusCodes.Status200OK);
+
+app.MapGet("api/orders/", (NorthwindContext db) => 
+    Results.Json(
+        db.Orders.Include(o => o.OrderDetails)))
+    .WithName("GetOrders")
+    .Produces<Order[]>(); // avoid the default 200 OK
 
 app.MapRazorPages();
 app.MapControllers();
--- Nothwind.BlazorLibraries/Shared/SharedClass.cs
+++ Nothwind.BlazorLibraries/Shared/SharedClass.cs
@@ -1,1 +1,10 @@
 /* Shared classes can be referenced by both the Client and Server */
+namespace Packt.Shared;
+
+public static class NorthwindExtensionMethods
+{
+    public static string ConvertToBase64Jpeg(this byte[] picture)
+    {
+        return $"data:image/jpg;base64,{Convert.ToBase64String(picture)}";
+    }
+}
