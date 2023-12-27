c53f5049fe5caae4990bf4372b69028e5efb55b4
jddios837 <jddios837@gmail.com>
2023-12-26 22:01:37 -0500

    Adding MVVM for Customer view

--- Chapter18/Northwind.Maui.Client/CustomersPage.xaml.cs
+++ Chapter18/Northwind.Maui.Client/CustomersPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Northwind.Maui.Client.ViewModels;
 
 namespace Northwind.Maui.Client;
 
@@ -11,5 +12,67 @@ public partial class CustomersPage : ContentPage
     public CustomersPage()
     {
         InitializeComponent();
+        
+        CustomersListViewModel viewModel = new();
+        viewModel.AddSampleData();
+        BindingContext = viewModel;
+    }
+
+    private async void Add_Clicked(object? sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CustomerDetailPage(
+            BindingContext as CustomersListViewModel));
+    }
+
+    private async void Customer_Tapped(object? sender, ItemTappedEventArgs e)
+    {
+        if (e.Item is not CustomerDetailViewModel c) return;
+
+        // navigate to the detail view and show the tapped customer
+        await Navigation.PushAsync(new CustomerDetailPage(
+            BindingContext as CustomersListViewModel, c));
+    }
+
+    private async void Customers_Refreshing(object? sender, EventArgs e)
+    {
+        if (sender is not ListView lv) return;
+        
+        lv.IsRefreshing = true;
+
+        await Task.Delay(1500);
+        
+        lv.IsRefreshing = false;
+    }
+
+    private async void Customer_Phoned(object? sender, EventArgs e)
+    {
+        MenuItem menuItem = sender as MenuItem;
+        if (menuItem.BindingContext is not CustomerDetailViewModel c) return;
+        if (await DisplayAlert("Dial a Number",
+                "Would you like to call " + c.Phone + "?",
+                "Yes", "No"))
+        {
+            try
+            {
+                if (PhoneDialer.IsSupported)
+                {
+                    PhoneDialer.Open(c.Phone);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert(title: "Failed",
+                    message: string.Format(
+                        "Failed to dial {0} due to: {1}", c.Phone, ex.Message),
+                    cancel: "OK");
+            }
+        }
+    }
+
+    private void Customer_Deleted(object? sender, EventArgs e)
+    {
+        MenuItem menuItem = sender as MenuItem;
+        if (menuItem.BindingContext is not CustomerDetailViewModel c) return;
+        (BindingContext as CustomersListViewModel).Remove(c);
     }
 }
---
+++ Chapter18/Northwind.Maui.Client/ViewModels/CustomerDetailViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Northwind.Maui.Client.ViewModels;
+
+public class CustomerDetailViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private string customerId;
+    private string companyName;
+    private string contactName;
+    private string city;
+    private string country;
+    private string phone;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    
+    public string CustomerId
+    {
+        get => customerId;
+        set
+        {
+            customerId = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public string CompanyName
+    {
+        get => companyName;
+        set
+        {
+            companyName = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public string ContactName
+    {
+        get => contactName;
+        set
+        {
+            contactName = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public string City
+    {
+        get => city;
+        set
+        {
+            city = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public string Country
+    {
+        get => country;
+        set
+        {
+            country = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public string Phone
+    {
+        get => phone;
+        set
+        {
+            phone = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public string Location
+    {
+        get => $"{City}, {Country}";
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
