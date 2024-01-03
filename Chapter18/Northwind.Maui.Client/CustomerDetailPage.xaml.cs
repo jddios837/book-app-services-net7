b84e7f882abbdcea64127344af193637ce8d289f
jddios837 <jddios837@gmail.com>
2024-01-02 20:55:24 -0500

    Adding Minimal API for MAUI APP Application

--- Chapter18/Northwind.Maui.Client/CustomerDetailPage.xaml.cs
+++ Chapter18/Northwind.Maui.Client/CustomerDetailPage.xaml.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Northwind.Maui.Client.ViewModels;
 
 namespace Northwind.Maui.Client;
 
 public partial class CustomerDetailPage : ContentPage
 {
-    public CustomerDetailPage()
+    private CustomersListViewModel customers;
+    
+    public CustomerDetailPage(CustomersListViewModel customers)
     {
         InitializeComponent();
+        
+        this.customers = customers;
+        BindingContext = new CustomerDetailViewModel();
+        Title = "New Customer";
+    }
+
+    public CustomerDetailPage(CustomersListViewModel customers, 
+        CustomerDetailViewModel customer)
+    {
+        InitializeComponent();
+        
+        this.customers = customers;
+        BindingContext = customer;
+        InsertButton.IsVisible = false;
+    }
+
+    private async void InsertButton_Clicked(object? sender, EventArgs e)
+    {
+        customers.Add((CustomerDetailViewModel)BindingContext);
+        await Navigation.PopAsync(animated: true);
     }
 }
