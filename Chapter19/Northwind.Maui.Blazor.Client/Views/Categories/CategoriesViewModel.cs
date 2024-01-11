3734da05b4b689f5dfef8ae7d1513915427d68cf
jddios837 <jddios837@gmail.com>
2024-01-11 16:46:00 -0500

    Adding MVVM in the .NET MAUI Blazor project

---
+++ Chapter19/Northwind.Maui.Blazor.Client/Views/Categories/CategoriesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Northwind.Maui.Blazor.Client.Views.Categories;
+
+internal partial class CategoriesViewModel : ObservableCollection<Category>
+{
+    public string InfoMessage { get; set; } = string.Empty;
+    public string ErrorMessage { get; set; } = string.Empty;
+    public bool ErrorMessageVisible { get; set; }
+
+    public CategoriesViewModel()
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ Chapter19/Northwind.Maui.Blazor.Client/Views/Categories/Category.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Northwind.Maui.Blazor.Client.Views.Categories;
+
+internal partial class Category : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PicturePath))]
+    private int categoryId;
+
+    [ObservableProperty]
+    private string categoryName;
+
+    [ObservableProperty]
+    private string description;
+    
+    [ObservableProperty]
+    private byte[] picture;
+    
+    [ObservableProperty]
+    private string picturePath;
+}
